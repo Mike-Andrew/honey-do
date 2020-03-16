9246e6e56a1258e0409e384a6d574d92a6d62eed
Mike Andrew <58096135+Mike-Andrew@users.noreply.github.com>
2020-03-16 14:18:38 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Header from './Components/layout/Header';
+import Todos from './Components/Todos';
+import AddTodo from './Components/AddTodo';
+import About from './Components/pages/About';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    todos: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10').then(res => this.setState({todos: res.data}))
+  }
+
+  //Toggle complete
+  markComplete = (id) => { 
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      } 
+      return todo;
+    })});
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
+  }
+
+  //Add Todo
+  addTodo = (title) => {
+    axios.post('https://jsonplaceholder.typicode.com/todos', 
+    { 
+      title: title,
+      completed: false
+    })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data]}));
+  }
+
+  render () {
+    return (
+      <Router>
+        <div className="App-header">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete}
+                delTodo={this.delTodo}/>
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ src/Components/layout/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+export function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>Honey-Do's</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
+        </header>
+    )
+}
+
+const headerStyle = {
+    backgroundColor: '#55a',
+    fontFamily: 'cursive',
+    fontSize: 'x-large',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px',
+    borderTopLeftRadius: '40px',
+    borderTopRightRadius: '40px'
+}
+
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none',
+    cursor: 'pointer'
+}
+
+export default Header;
