d966cd26282c49c801c5ee4cc9a7254ac0ff698e
Mike Andrew <58096135+Mike-Andrew@users.noreply.github.com>
2020-03-16 14:41:31 -0500

    Updates

---
+++ precache-manifest.cbbdd5e95c9521cb8fb33fa644300a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5ac6aafff8935c6b27cda86d7d796b",
+    "url": "/Projects/to-do-app/index.html/index.html"
+  },
+  {
+    "revision": "5abc38f8ea36650fa79c",
+    "url": "/Projects/to-do-app/index.html/static/css/main.6118055a.chunk.css"
+  },
+  {
+    "revision": "fc06a8e26efc57d6a051",
+    "url": "/Projects/to-do-app/index.html/static/js/2.da8afcce.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Projects/to-do-app/index.html/static/js/2.da8afcce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abc38f8ea36650fa79c",
+    "url": "/Projects/to-do-app/index.html/static/js/main.0286e973.chunk.js"
+  },
+  {
+    "revision": "48648ed45db3b9651d7b",
+    "url": "/Projects/to-do-app/index.html/static/js/runtime-main.dce68c0a.js"
+  }
+]);
