03b97008df225ee58b0a9578770745c9608a569b
Mike Andrew <58096135+Mike-Andrew@users.noreply.github.com>
2020-03-16 15:01:30 -0500

    c

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Projects/to-do-app/index.html/precache-manifest.cbbdd5e95c9521cb8fb33fa644300a83.js"
+  "/honey-do/index.html/precache-manifest.29be9aa8922e3a9ce8f79adb7b33691c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Projects/to-do-app/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/honey-do/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
